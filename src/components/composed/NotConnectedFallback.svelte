158e456f1af9a75049674bba0ba793eb2c0bc542
hone1er <24376928+hone1er@users.noreply.github.com>
2024-04-18 11:40:04 +0200

    improve spacing

--- src/components/composed/NotConnectedFallback.svelte
+++ src/components/composed/NotConnectedFallback.svelte
@@ -4,18 +4,20 @@
 </script>
 
 <div
-  class="flex flex-col justify-center align-center items-center content-center text-center max-w-[600px] mx-auto gap-4 my-10 lg:mt-40"
+  class="flex flex-col justify-center align-center items-center content-center text-center max-w-[600px] mx-auto my-10 lg:mt-40"
 >
   <ContainerWithHeader>
-    <div slot="body" class="w-full place-self-center p-8 lg:p-20">
-      <h2 class="text-white text-xl">Please, connect your wallet</h2>
-      <p class="text-white text-sm text-center">
-        We couldn't detect a wallet.
-        <br />
-        Connect your wallet to continue.
-      </p>
-      <div class="max-w-[216px] mx-auto">
-        <Wallet />
+    <div slot="body" class="w-full place-self-center p-4 lg:p-10">
+      <div class="flex flex-col gap-6">
+        <h2 class="text-white text-xl">Please, connect your wallet</h2>
+        <p class="text-white text-sm text-center">
+          We couldn't detect a wallet.
+          <br />
+          Connect your wallet to continue.
+        </p>
+        <div class="max-w-[216px] mx-auto">
+          <Wallet />
+        </div>
       </div>
     </div>
   </ContainerWithHeader>
