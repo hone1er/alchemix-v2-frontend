b728eb7dd4007d01bd02aa53cd03cce0e7687666
hone1er <24376928+hone1er@users.noreply.github.com>
2024-04-18 11:37:15 +0200

    center the not connected fallback

--- src/components/composed/NotConnectedFallback.svelte
+++ src/components/composed/NotConnectedFallback.svelte
@@ -1,21 +1,22 @@
 <script>
   import ContainerWithHeader from '../elements/ContainerWithHeader.svelte';
-  import ViewContainer from '../elements/ViewContainer.svelte';
   import Wallet from './Wallet.svelte';
 </script>
 
-<ContainerWithHeader>
-  <div slot="body" class="w-full place-self-center">
-    <div class="flex flex-col justify-center align-center items-center my-4 max-w-[360px] mx-auto gap-4">
+<div
+  class="flex flex-col justify-center align-center items-center content-center text-center max-w-[600px] mx-auto gap-4 my-10 lg:mt-40"
+>
+  <ContainerWithHeader>
+    <div slot="body" class="w-full place-self-center p-8 lg:p-20">
       <h2 class="text-white text-xl">Please, connect your wallet</h2>
       <p class="text-white text-sm text-center">
         We couldn't detect a wallet.
         <br />
         Connect your wallet to continue.
       </p>
-      <div class="max-w-[216px]">
+      <div class="max-w-[216px] mx-auto">
         <Wallet />
       </div>
     </div>
-  </div>
-</ContainerWithHeader>
+  </ContainerWithHeader>
+</div>
